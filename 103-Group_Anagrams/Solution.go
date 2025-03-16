c4d912bb1c7a0db7c7c6913c6b36013db60cc659
alecmoran7 <48536590+alecmoran7@users.noreply.github.com>
2025-03-16 01:03:50 -0600

    Fix double-pasted file

--- 103-Group_Anagrams/Solution.go
+++ 103-Group_Anagrams/Solution.go
@@ -1,45 +1,4 @@
-mport (
-    "fmt"
-    "sort"
-    "slices"
-)
-
-func getSortedString(stringToSort string) string{
-        runeArray := []rune(stringToSort)
-        sort.Slice(runeArray, func(j, k int) bool {
-            return runeArray[j] < runeArray[k]
-        })
-        return string(runeArray)
-}
-
-func groupAnagrams(strs []string) [][]string {
-    // fmt.Println("strs: ", strs)
-    var answerArray = make([][]string, 0)
-    var indicesToSkip = make([]int, 0)
-        for i := 0; i < len(strs); i++ {
-            if slices.Contains(indicesToSkip, i) {
-                // fmt.Println("SKIPPING ", strs[i])
-                continue // skip adding strings that have already been assigned to a group
-            }
-            if i == len(strs) - 1 {
-                newArray := make([]string, 0)
-                newArray = append(newArray, strs[i])
-                answerArray = append(answerArray, newArray)
-                break
-            }
-            var stringMatch = getSortedString(strs[i])
-            newArray := make([]string, 0)
-            newArray = append(newArray, strs[i])
-            for j:= i + 1; j < len(strs); j++ {
-                if stringMatch == getSortedString(strs[j]){
-                    newArray = append(newArray, strs[j])
-                    indicesToSkip = append(indicesToSkip, j)
-                }
-            }
-            answerArray = append(answerArray, newArray)
-        }
-    return answerArray
-}mport (
+import (
     "fmt"
     "sort"
     "slices"
